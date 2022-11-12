da66f5f1fad869b03807f78f4222da5bb054362f
purreB <jakob.bergstrom@live.se>
2022-11-12 10:37:26 +0100

    added services

--- Omegapoint.Core/Services/PdfService.cs
+++ Omegapoint.Core/Services/PdfService.cs
@@ -1,5 +1,46 @@
-﻿namespace Services;
-public class Class1
-{
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Contracts;
+using Domain.Entities;
+using Domain.Exceptions;
+using Domain.RepositoryInterface;
+using Mapster;
+using Services.Abstractions;
 
+namespace Services;
+
+internal sealed class PdfService : IPdfService
+{
+  private readonly IRepositoryManager _repositoryManager;
+  public PdfService(IRepositoryManager repositoryManager) => _repositoryManager = repositoryManager;
+  public async Task<IEnumerable<PdfDto>> GetAllAsync(CancellationToken cancellationToken = default)
+  {
+    var pdfs = await _repositoryManager.PdfRepository.GetAllAsync(cancellationToken);
+    var pdfsDto = pdfs.Adapt<IEnumerable<PdfDto>>();
+    return pdfsDto;
+  }
+
+  public async Task<PdfDto> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+  {
+    var pdf = await _repositoryManager.PdfRepository.GetByIdAsync(id, cancellationToken);
+    if (pdf is null)
+    {
+      throw new PdfNotFoundException(id);
+    }
+
+    var pdfDto = pdf.Adapt<PdfDto>();
+    return pdfDto;
+  }
+
+  public async Task<PdfDto> CreateAsync(PdfForCreationDto pdfForCreationDto, CancellationToken cancellationToken = default)
+  {
+    //* Add Error Handling here??
+    var pdf = pdfForCreationDto.Adapt<Pdf>();
+    _repositoryManager.PdfRepository.Insert(pdf);
+    await _repositoryManager.UnitOfWork.SaveChangesAsync(cancellationToken);
+    return pdf.Adapt<PdfDto>();
+  }
 }
+
--- Omegapoint.Core/Services/ServiceManager.cs
+++ Omegapoint.Core/Services/ServiceManager.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Services.Abstractions;
+using Domain.RepositoryInterface;
 
 namespace Services
 {
-    public class ServiceManager
+  public sealed class ServiceManager : IServiceManager
+  {
+    private readonly Lazy<IPdfService> _lazyPdfService;
+    public ServiceManager(IRepositoryManager repositoryManager)
     {
-        
+      _lazyPdfService = new Lazy<IPdfService>(() => new PdfService(repositoryManager));
     }
+    public IPdfService PdfService => _lazyPdfService.Value;
+  }
 }
